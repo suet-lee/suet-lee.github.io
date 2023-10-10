15c623ed899ba3a463e90f69a9adb06188fdd8f0
Suet <suet.lee@bristol.ac.uk>
2023-10-10 14:19:27 +0100

    Adding test

--- assets/js/canvas_bg.js
+++ assets/js/canvas_bg.js
@@ -1,3 +1,75 @@
-// $("#canvas-bg").
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-console.log('tset')
+// Recursive function to draw branches
+function drawBranch(x, y, length, angle, depth) {
+if (depth === 0) return;
+
+const newX = x + Math.cos(angle) * length;
+const newY = y + Math.sin(angle) * length;
+
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.lineTo(newX, newY);
+ctx.strokeStyle = 'green';
+ctx.lineWidth = depth;
+ctx.stroke();
+
+// Recursively draw the child branches
+drawBranch(newX, newY, length * 0.7, angle - 0.3, depth - 1);
+drawBranch(newX, newY, length * 0.7, angle + 0.3, depth - 1);
+}
+
+// Start drawing the plant
+const startX = canvas.width / 2;
+const startY = canvas.height;
+const initialLength = 100;
+const initialAngle = -Math.PI / 2;
+const initialDepth = 8;
+
+drawBranch(startX, startY, initialLength, initialAngle, initialDepth)
+
+
+// G = {
+//     nodes: 5,
+//     canvas_w: 100,
+//     canvas_h: 100,
+//     iteration: 0,
+//     max_iteration: 100000,
+//     active_nodes: []
+// }
+
+// function resizeCanvas()
+// {
+//     var canvas = document.querySelector("#canvas-bg")   // Get access to HTML canvas element
+//     var ctx = canvas.getContext("2d")
+//     G.canvas_w = canvas.width = window.innerWidth
+//     G.canvas_h = canvas.height = window.innerHeight
+// }
+
+// // Randomly initialise active nodes
+// function initNodes()
+// {
+//     for (i)
+//     Math.random()
+//     G.canvas_w
+// }
+
+// function drawFrame()
+// {
+
+// }
+
+
+// resizeCanvas()
+// initNodes()
+// interval = window.setInterval(function(){
+    
+//     G.i += 1
+//     if (G.i >= G.max_i) {
+//         clearInterval(interval)
+//     }
+    
+//     drawFrame()
+    
+// }, 5);       
